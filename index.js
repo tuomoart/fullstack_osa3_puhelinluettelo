b7b9507e57557a9181b40bccde7beccb4565e20d
Tuomo Artama <tuomo.artama@gmail.com>
2021-02-22 18:45:47 +0200

    frontend sekaan

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const static = require('static')
 const app = express()
 
 app.use(express.json()) 
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('content', function (req, res) {return JSON.stringify(req.body)})
 app.use(morgan(function (tokens, req, res) {

a2ecb7f441b718db6864503b223df6f6843d408f
Tuomo Artama <tuomo.artama@gmail.com>
2021-02-22 17:59:20 +0200

    ready for heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json()) 
+app.use(cors())
 
 morgan.token('content', function (req, res) {return JSON.stringify(req.body)})
 app.use(morgan(function (tokens, req, res) {
@@ -97,7 +99,7 @@ app.post('/api/persons', (request, response) => {
 })
   
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
     app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

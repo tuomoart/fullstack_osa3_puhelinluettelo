bfe00f378c0e23da80ee61b2793864b3b2d520ea
Tuomo Artama <tuomo.artama@gmail.com>
2021-02-22 17:35:11 +0200

    3.8 asti done

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json()) 
+
+morgan.token('content', function (req, res) {return JSON.stringify(req.body)})
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.content(req, res)
+    ].join(' ')
+}))
 
 let persons = [
     {

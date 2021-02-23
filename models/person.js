6274424135184e3fefb6ac2050c5b12c28aaec91
Tuomo Artama <tuomo.artama@gmail.com>
2021-02-23 15:37:34 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -16,11 +16,13 @@ const personSchema = new mongoose.Schema({
     name: {
         type: String,
         unique: true,
-        required: true
+        required: true,
+        minlength: 3
     }, 
     number: {
         type: String,
-        required: true
+        required: true,
+        minlength: 8
     }
 })
 

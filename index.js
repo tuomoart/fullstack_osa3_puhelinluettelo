cae93d3e7c8bfa06878b43a3c9670de6aa7eff39
Tuomo Artama <tuomo.artama@gmail.com>
2021-02-23 13:11:06 +0200

    3.18 asti done

--- index.js
+++ index.js
@@ -57,23 +57,25 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        console.log(person)
         if (person) {
             response.json(person)
         } else {
             response.status(404).end()
         }
     })
+    .catch(error => next(error))
     
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+    
 })
   
 app.post('/api/persons', (request, response) => {
@@ -102,9 +104,50 @@ app.post('/api/persons', (request, response) => {
         response.json(person)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) =>{
+    const body = request.body
+  
+    if (!body.name) {
+        return response.status(400).json({ 
+            error: 'name missing' 
+        })
+    } else if (!body.number) {
+        return response.status(400).json({ 
+            error: 'number missing' 
+        })
+    } else if (persons.map(p => p.name).includes(body.name)) {
+        return response.status(400).json({ 
+            error: `name '${body.name}' is already found in phonebook`
+        })
+    }
+  
+    const person = {
+      name: body.name,
+      number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new:true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+}
+
+app.use(errorHandler)
   
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
     app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 }) 
